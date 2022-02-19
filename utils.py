e9313eda4c8e40a36e32a9e44e1c07af52ab3a28
Szymon Grzebyta <szymon.grzebyta@olx.pl>
2022-02-19 14:32:29 +0100

    added coments

---
+++ utils.py
@@ -0,0 +1,263 @@
+import pandas as pd
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.datasets import load_digits
+from sklearn.model_selection import learning_curve
+from sklearn.model_selection import ShuffleSplit
+
+from sklearn.naive_bayes import GaussianNB 
+from xgboost import XGBClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import MinMaxScaler
+
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_predict
+import plotly.express as px
+import datetime
+from collections import defaultdict
+from tqdm import tqdm
+pd.options.mode.chained_assignment = None
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+from sklearn.metrics import f1_score
+from sklearn.metrics import  confusion_matrix, classification_report
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import MinMaxScaler
+
+
+def make_dummy_from_list(df, col_name):
+    """
+    input : df with colulmn with list of values
+    
+    output: df with one hot encoding approach
+    """
+
+    dummy_df = pd.get_dummies(df[col_name].apply(pd.Series).stack(), prefix= col_name).sum(level=0)
+    
+    final_df = pd.concat([df, dummy_df], axis=1, sort=False)
+    final_df.drop(columns=col_name, inplace=True)
+    return final_df
+
+
+def assing_effect(row):
+    
+    if row['1: received time'] <= row['2: viewed time'] <= row['3: completed time']:
+        
+        return 'effctive_success', (row['3: completed time'] - row['2: viewed time'])
+
+        
+    elif row['1: received time'] <= row['3: completed time']:
+        
+#         return 'unaware_success', (row['3: completed time'] - row['1: received time'])
+        return 'unaware_success', 0
+        
+        
+    elif ( pd.isna(row['2: viewed time']) and pd.isna(row['3: completed time']) ):
+        
+        return 'no_action', 0
+    
+    
+    elif pd.isna(row['3: completed time']):
+        return 'viewed', ((row['1: received time'] + row['duration']) - row['2: viewed time'])
+            
+    else:
+        return 'other', 0
+    
+
+def train_predict_result(model, X_train, y_train, X_test, y_test, plot = False):
+    print("{}".format(model.__class__.__name__))
+    
+    results = {}
+    
+    #fit the model
+    model.fit(X_train, y_train)
+    
+    #predict test
+    y_pred_test = model.predict(X_test)
+    
+    #predict train
+    y_pred_train = model.predict(X_train)
+    
+    # plot
+    if plot:
+        sorted_idx = model.feature_importances_.argsort()
+        plt.barh(X_train.columns[sorted_idx], model.feature_importances_[sorted_idx])
+        plt.show()
+    
+    #Accuracy of training
+    results['acc_train'] = accuracy_score(y_train, y_pred_train)
+    
+    #Accuracy of testing
+    results['acc_test'] = accuracy_score(y_test, y_pred_test)
+    
+    #f1-score for training
+    results['f1_train'] = f1_score(y_train, y_pred_train)
+    
+    #f1-score for testing
+    results['f1_test'] = f1_score(y_test, y_pred_test)
+        
+    # Return the results
+    return results
+
+
+def plot_learning_curve(
+    estimator,
+    title,
+    X,
+    y,
+    axes=None,
+    ylim=None,
+    cv=None,
+    n_jobs=None,
+    train_sizes=np.linspace(0.1, 1.0, 5),
+):
+    """
+    Generate 3 plots: the test and training learning curve, the training
+    samples vs fit times curve, the fit times vs score curve.
+
+    Parameters
+    ----------
+    estimator : estimator instance
+        An estimator instance implementing `fit` and `predict` methods which
+        will be cloned for each validation.
+
+    title : str
+        Title for the chart.
+
+    X : array-like of shape (n_samples, n_features)
+        Training vector, where ``n_samples`` is the number of samples and
+        ``n_features`` is the number of features.
+
+    y : array-like of shape (n_samples) or (n_samples, n_features)
+        Target relative to ``X`` for classification or regression;
+        None for unsupervised learning.
+
+    axes : array-like of shape (3,), default=None
+        Axes to use for plotting the curves.
+
+    ylim : tuple of shape (2,), default=None
+        Defines minimum and maximum y-values plotted, e.g. (ymin, ymax).
+
+    cv : int, cross-validation generator or an iterable, default=None
+        Determines the cross-validation splitting strategy.
+        Possible inputs for cv are:
+
+          - None, to use the default 5-fold cross-validation,
+          - integer, to specify the number of folds.
+          - :term:`CV splitter`,
+          - An iterable yielding (train, test) splits as arrays of indices.
+
+        For integer/None inputs, if ``y`` is binary or multiclass,
+        :class:`StratifiedKFold` used. If the estimator is not a classifier
+        or if ``y`` is neither binary nor multiclass, :class:`KFold` is used.
+
+        Refer :ref:`User Guide <cross_validation>` for the various
+        cross-validators that can be used here.
+
+    n_jobs : int or None, default=None
+        Number of jobs to run in parallel.
+        ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
+        ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
+        for more details.
+
+    train_sizes : array-like of shape (n_ticks,)
+        Relative or absolute numbers of training examples that will be used to
+        generate the learning curve. If the ``dtype`` is float, it is regarded
+        as a fraction of the maximum size of the training set (that is
+        determined by the selected validation method), i.e. it has to be within
+        (0, 1]. Otherwise it is interpreted as absolute sizes of the training
+        sets. Note that for classification the number of samples usually have
+        to be big enough to contain at least one sample from each class.
+        (default: np.linspace(0.1, 1.0, 5))
+    """
+    if axes is None:
+        _, axes = plt.subplots(1, 3, figsize=(20, 5))
+
+    axes[0].set_title(title)
+    if ylim is not None:
+        axes[0].set_ylim(*ylim)
+    axes[0].set_xlabel("Training examples")
+    axes[0].set_ylabel("Score")
+
+    train_sizes, train_scores, test_scores, fit_times, _ = learning_curve(
+        estimator,
+        X,
+        y,
+        cv=cv,
+        n_jobs=n_jobs,
+        train_sizes=train_sizes,
+        return_times=True,
+    )
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    fit_times_mean = np.mean(fit_times, axis=1)
+    fit_times_std = np.std(fit_times, axis=1)
+
+    # Plot learning curve
+    axes[0].grid()
+    axes[0].fill_between(
+        train_sizes,
+        train_scores_mean - train_scores_std,
+        train_scores_mean + train_scores_std,
+        alpha=0.1,
+        color="r",
+    )
+    axes[0].fill_between(
+        train_sizes,
+        test_scores_mean - test_scores_std,
+        test_scores_mean + test_scores_std,
+        alpha=0.1,
+        color="g",
+    )
+    axes[0].plot(
+        train_sizes, train_scores_mean, "o-", color="r", label="Training score"
+    )
+    axes[0].plot(
+        train_sizes, test_scores_mean, "o-", color="g", label="Cross-validation score"
+    )
+    axes[0].legend(loc="best")
+
+    # Plot n_samples vs fit_times
+    axes[1].grid()
+    axes[1].plot(train_sizes, fit_times_mean, "o-")
+    axes[1].fill_between(
+        train_sizes,
+        fit_times_mean - fit_times_std,
+        fit_times_mean + fit_times_std,
+        alpha=0.1,
+    )
+    axes[1].set_xlabel("Training examples")
+    axes[1].set_ylabel("fit_times")
+    axes[1].set_title("Scalability of the model")
+
+    # Plot fit_time vs score
+    fit_time_argsort = fit_times_mean.argsort()
+    fit_time_sorted = fit_times_mean[fit_time_argsort]
+    test_scores_mean_sorted = test_scores_mean[fit_time_argsort]
+    test_scores_std_sorted = test_scores_std[fit_time_argsort]
+    axes[2].grid()
+    axes[2].plot(fit_time_sorted, test_scores_mean_sorted, "o-")
+    axes[2].fill_between(
+        fit_time_sorted,
+        test_scores_mean_sorted - test_scores_std_sorted,
+        test_scores_mean_sorted + test_scores_std_sorted,
+        alpha=0.1,
+    )
+    axes[2].set_xlabel("fit_times")
+    axes[2].set_ylabel("Score")
+    axes[2].set_title("Performance of the model")
+
+    return plt
